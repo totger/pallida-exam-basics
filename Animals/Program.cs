5aa3eb73b25ed0106ca775ae8da082606e7d25a5
Gergo Toth <toothgergo@gmail.com>
2017-10-06 11:55:21 +0200

    feat(Animals) write arguments to file

---
+++ Animals/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Animals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"../../favourites.txt";
+
+            if (args == null)
+            {
+                Console.WriteLine("C# favourite animals: ");
+            }
+            else
+            {
+                List<string> animals = File.ReadAllLines(path).ToList();
+
+                foreach (string argument in args)
+                {
+                    if (!animals.Contains(argument))
+                    {
+                        using (StreamWriter writer = File.AppendText(path)) 
+                        {
+							writer.WriteLine(argument);    
+                        }
+                    }
+                }   
+            }
+        }
+    }
+}
