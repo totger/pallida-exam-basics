f07ea0709aa57090e9fa5ed9747fd014851ee983
Gergo Toth <toothgergo@gmail.com>
2017-10-06 10:58:53 +0200

    feat(UniqueChars): input to char list

---
+++ UniqueCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniqueCharacters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UniqueChars("anagram");
+        }
+        public static List<char> UniqueChars(string input)
+        {
+            List<char> characters = new List<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                characters.Add(input[i]);
+            }
+            List<char> returnList = new List<char>();
+        }
+    }
+}
